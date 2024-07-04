67fe6d559fcecf826214fc412646975894ce5103
YF <yfp16443@gmail.com>
2024-07-04 01:19:36 +0000

    new commit

--- cifar100/train-org.sh
+++ cifar100/train-org.sh
@@ -3,7 +3,7 @@
 #SBATCH --mem=40000  # Requested Memory
 #SBATCH -G 1  # Number of GPUs
 #SBATCH -t 10:00:00  # Job time limit
-#SBATCH --partition gpu-preempt  # Job time limit
+#SBATCH --partition gpu-long  # Job time limit
 #SBATCH -o ./training_log/sbatch/slurm-%j.out  # %j = job ID
 
 mkdir -p ./training_log/
