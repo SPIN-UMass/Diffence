d42670b03bf6d8dfca5869da25377c30967f0027
YF <yfp16443@gmail.com>
2024-07-15 01:07:18 +0000

    new commit

--- svhn/diff_defense/purification/purify_imagenet.py
+++ svhn/diff_defense/purification/purify_imagenet.py
@@ -7,14 +7,18 @@ import torch.nn.functional as F
 
 def purify_imagenet(x, diffusion,model,  max_iter, mode, config):
     # From noisy initialized image to purified image
+    purify_step=config.purification.purify_step+1 #step list starts from 0
     images_list = []
     transform_raw_to_diff = raw_to_diff(config.structure.dataset)
     transform_diff_to_raw = diff_to_raw(config.structure.dataset)
     x_adv = transform_raw_to_diff(x).to(config.device.diff_device)
-    x_adv = torch.nn.functional.interpolate(x_adv, size=[256, 256], mode="bilinear")  # transfrom size 224 -> 256
+    # x_adv = torch.nn.functional.interpolate(x_adv, size=[256, 256], mode="bilinear")  # transfrom size 224 -> 256
+    origin_size =x_adv.size()[-2:]
+    resize_transform = transforms.Resize((256, 256))
+    x_adv = resize_transform(x_adv)
 
     t_steps = torch.ones(x_adv.shape[0], device=config.device.diff_device).long()
-    t_steps = t_steps * (config.purification.purify_step-1)
+    t_steps = t_steps * (purify_step-1)
     shape = list(x_adv.shape)
     model_kwargs = {}
 
@@ -28,7 +32,7 @@ def purify_imagenet(x, diffusion,model,  max_iter, mode, config):
             
             # x_adv_t = diffusion.q_sample(x_adv, t)
             x_adv_t = x_adv
-            # scale = exp(config.purification.guide_exp_a * t / config.purification.purify_step+config.purification.guide_exp_b) + config.purification.guide_scale_base
+            # scale = exp(config.purification.guide_exp_a * t / purify_step+config.purification.guide_exp_b) + config.purification.guide_scale_base
             if config.purification.guide_mode == 'MSE': 
                 selected = -1 * F.mse_loss(x_in, x_adv_t)
                 scale = diffusion.compute_scale(x_in,t, config.attack.ptb*2/255. / 3. / config.purification.guide_scale)
@@ -40,6 +44,7 @@ def purify_imagenet(x, diffusion,model,  max_iter, mode, config):
                 scale = config.purification.guide_scale
             return torch.autograd.grad(selected.sum(), x_in)[0] * scale
 
+
     with torch.no_grad():
         images = []
         xt_reverse = x_adv
@@ -49,7 +54,7 @@ def purify_imagenet(x, diffusion,model,  max_iter, mode, config):
             xt_reverse = sample_fn(
                     model,
                     shape,
-                    num_purifysteps = config.purification.purify_step,
+                    num_purifysteps = purify_step,
                     noise = adv_sample,
                     clip_denoised=config.net.clip_denoised,
                     cond_fn = cond_fn if config.purification.cond else None,
@@ -59,7 +64,10 @@ def purify_imagenet(x, diffusion,model,  max_iter, mode, config):
             # x_pur_t = xt_reverse.clone()
             x_pur = torch.clamp(transform_diff_to_raw(x_pur_t), 0.0, 1.0)
             # x_pur = torch.nn.functional.interpolate(x_pur, size=[224, 224], mode="bilinear") # transfrom size 256 -> 224
-            x_pur = torch.nn.functional.interpolate(x_pur, size=[32, 32], mode="bilinear") # transfrom size 256 -> 224
+            # x_pur = torch.nn.functional.interpolate(x_pur, size=[218, 178], mode="bilinear") # transfrom size 256 -> 224
+            reverse_resize_transform = transforms.Resize(origin_size)
+            x_pur = reverse_resize_transform(x_pur)
+
             images.append(x_pur)
 
     return images
